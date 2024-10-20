5c508e12b332a0253cdaaeaa9f30919b5a3f6388
hieu.tran <trunghieu27102002@gmail.com>
2024-10-20 22:16:03 +0700

    feat(login-page): create ui login page

--- visulization-player/client/src/app/components/login-page/login-page.component.ts
+++ visulization-player/client/src/app/components/login-page/login-page.component.ts
@@ -1,13 +1,26 @@
 import { Component, ElementRef, OnDestroy, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 
+
 @Component({
     selector: 'login-page',
     templateUrl: './login-page.component.html',
     styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
+    public valuePassword = ''
+    public showPassword: boolean = false
+
     constructor() { }
 
     ngOnInit(): void {
+    }
+
+    handleResetPw() {
+        this.valuePassword = ''
+        console.log('running ')
+    }
+
+    handleShowPassword() {
+        this.showPassword = !this.showPassword
     }
 }
